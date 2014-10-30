a6dc0c78f9af5eafa3d50689318c4a139a28b7ac
Matt Thompson <thompsonson@gmail.com>
2014-10-30 20:37:13 +0000

    hooray! got a OOP(ish) Knockout.js Model setup.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,60 @@
-
-var db = new PouchDB('./db');
-console.log(db.adapter); // prints either 'idb' or 'websql'
-
-db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-	console.log(doc.rows);
-});
-
-function MetricValue(date, name, value, comment) {
-	this._id = new Date().toISOString();
-	this.date = date;
-	this.name = name;
-	this.value = value;
-	this.comment = comment || '';
+function MetricValue(args) {
+	this._id = ko.observable(args.id || new Date().toISOString());
+	this.date = ko.observable(args.date);
+	this.name = ko.observable(args.name);
+	this.value = ko.observable(args.value);
+	this.comment = ko.observable(args.comment || '');
 }
-
-
+ 
+function ModelCollection(options){
+	this.db = new PouchDB(options.db || './db');
+	//console.log(db.adapter); // prints either 'idb' or 'websql'
+ 
+	this.DataArray = ko.observableArray();
+	this.lastSavedJson = ko.computed(function() {       
+		return JSON.stringify(ko.toJS(this.DataArray, null, 2));  
+	}, this);
+} ;
+ 
+ModelCollection.prototype.put = function(object) {
+	that = this;
+ 
+	this.object = object;
+	this.nonObservableObj = ko.toJS(object);
+ 
+	this.db.put(this.nonObservableObj, function callback(err, doc){
+		if (!err) {
+			console.log('Successfully posted');
+			that.DataArray.push(that.object);
+		} else {
+			console.log("error");
+			console.log(err);
+		}
+	});
+}
+ 
+ModelCollection.prototype.getAllMetrics = function() {
+	that = this;
+               
+	this.db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+		if (!err) {
+		    doc.rows.forEach(function(row) {
+			//console.log(row.doc);
+			// any way to get this seperated from the actual model so it can be anything... i.e. new Object_Of_Type(type) or similar rather than new MetricValue()
+			// actually doesn't need it as it's the "getAllMetrics" function!! still an interesting concept for a get all...
+				that.DataArray.push(new MetricValue(row.doc));
+		    });
+		} else {
+			console.log("error");
+			console.log(err);
+		}
+	});
+ 
+}
+ 
+var model = new ModelCollection({});
+model.getAllMetrics();
+ 
 var gui = require('nw.gui');
 win = gui.Window.get();
 var nativeMenuBar = new gui.Menu({ type: "menubar" });
@@ -22,36 +62,42 @@ try {
 	nativeMenuBar.createMacBuiltin("My App");
 	win.menu = nativeMenuBar;
 } catch (ex) {
-	console.log(ex.message);
+	//console.log(ex.message);
 }
-
+ 
 var myForm, formData;
 formData = [
 	{type: "settings", position: "label-top", labelWidth: 130, inputWidth: 120},
-	{type: "fieldset", label: "Enter Metric", inputWidth: 420, list:[
+	{type: "fieldset", label: "Enter Metric", inputWidth: 550, list:[
 		{type:"calendar", label:"Date", skin:"dhx_skyblue", name: "metric_date", enableTime:true, dateFormat:"%Y-%m-%d %H:%i"},
+ 
 		{type:"newcolumn"},
 		{type: "input", label: "Name", name: "metric_name", value: "Situps"},
 		{type:"newcolumn"},
 		{type: "input", label: "Value", name: "metric_value", value: "10", inputWidth: 50},
-
+		{type:"newcolumn"},
+		{type: "input", label: "Unit", name: "metric_unit", value: "", inputWidth: 50},
+ 
 		{type: "button", value: "Submit", name: "Submit"}
+ 
 	]}
 ];
-
-myForm = new dhtmlXForm("myForm", formData);	
-
+ 
+myForm = new dhtmlXForm("myForm", formData);       
+ 
 myForm.attachEvent("onButtonClick", function(id){
 	console.log("button clicked: " + id);
 	if (id=="Submit") {
-		var entry = new MetricValue(myForm.getItemValue("metric_date").getTime(), myForm.getItemValue("metric_name"), myForm.getItemValue("metric_value"))
-		console.log(entry);
-
-		db.put(entry, function callback(err, result) {
-			if (!err) {
-				console.log('Successfully posted a metric!');
-			}
+		var entry = new MetricValue({
+			date: myForm.getItemValue("metric_date").getTime(),
+			name: myForm.getItemValue("metric_name"),
+			value: myForm.getItemValue("metric_value")
 		});
-
+		console.log(entry);
+ 
+		model.put(entry);
 	}
 });
+ 
+ko.applyBindings(model);
+ 
